efe8483509c41d5d230f3487aeada4e2af2ec796
Phariq <pharisnjuguna22@gmail.com>
2022-11-01 15:59:44 +0300

    Emoji app

---
+++ main.js
@@ -0,0 +1,20 @@
+const openFace = document.querySelector('.open');
+const closedFace = document.querySelector('.closed');
+
+const revealMessage = () => {
+    document.querySelector('#hiddenContent').style.display = 'block';
+} 
+
+closedFace.addEventListener('click', () => {
+    if(openFace.classList.contains('open')){
+        openFace.classList.add('active');
+        closedFace.classList.remove('active');
+    }
+});
+
+openFace.addEventListener('click', () => {
+    if(closedFace.classList.contains('closed')){
+        closedFace.classList.add('active');
+        openFace.classList.remove('active');
+    }
+})
